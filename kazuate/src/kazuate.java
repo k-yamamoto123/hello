ae95ed7bfb92b235c8b68273c4b6a8a6c546d11f
k-yamamoto123 <49426718+k-yamamoto123@users.noreply.github.com>
2020-06-29 13:13:16 +0900

    'kazuateゲーム修正2'

--- kazuate/src/kazuate.java
+++ kazuate/src/kazuate.java
@@ -3,33 +3,34 @@ import java.util.Scanner;
 public class kazuate{
     public static void main(String[] args){
         int num = 25;//正の2桁の整数
+        final int chance = 5;//最大試行回数
 
         Scanner scanner = new Scanner(System.in);
 
         int ans = 0;
 
-        for(int i=4;i>=0;i--){
+        for(int i=chance - 1; i >= 0; i--){
 
-            System.out.println("2桁の整数を入力してください:");
+            System.out.print("2桁の整数を入力してください:");
             ans = scanner.nextInt(); 
 
             if(ans == num){
                 System.out.println("正解です！");
                 break;
             }
-            else if(Math.abs(ans - num) >= 20){
+            if(Math.abs(ans - num) >= 20){
                 System.out.println("20以上の差があります");
             }
-            else if(ans > num){
+            if(ans > num){
                 System.out.println("正解より大きいです");
             }
-            else if(num > ans){
+            else if(ans < num){
                 System.out.println("正解より小さいです");
             }
 
             System.out.println("残りのチャンスは"+ i + "回です");
         }
-        
+
         System.out.println("ゲームを終了します");
         scanner.close();
 
